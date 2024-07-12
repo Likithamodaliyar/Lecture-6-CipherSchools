438aa229c5ee02010aa941826d59adbe5dc9b315
Likitha Modaliyar <119063547+Likithamodaliyar@users.noreply.github.com>
2024-07-12 21:22:54 +0530

    Add files via upload

---
+++ Lecture 6.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+using namespace std;
+int main(){
+//	float a;
+//	cin>>a;
+	
+//	if(a>0.01)
+//	{
+//		cout<<"The condition in if statement is correct";
+//	}
+
+
+// Taking 2 imputs 
+
+// float a,b;
+//   cout<<"floating point numbers as input"<<endl;
+ //  cin>>a;
+//   cin>>b;
+   
+//   if(a>0.01 && b>a){
+//		cout<<"The condition in if statement is correct";
+//	}
+// else{
+//	cout<<"The statement is False";
+//}
+   
+
+//marks conditional statement using if-else condition
+// int marks;
+// cout<<"students,please enter the marks to calculate the grade";
+// cin>>marks;
+
+// if(marks>90){
+// 	cout<<"A";
+// }
+
+// else if(marks>80){
+// 	cout<<"B";
+// }
+
+// else if(marks>70){
+// 	cout<<"C";
+// }
+
+// else if(marks>60){
+//	cout<<"D";
+// }
+
+// else{
+//	cout<<"pass";
+//   }
+
+int num;
+cout<<"Enter integer a";
+cin>>num;
+switch(num){
+	case 1:
+	cout<<"The value of a is 1";
+	break;
+	
+	case 2:
+	cout<<"The value of a is 2";
+	break;
+	
+	case 3:
+	cout<<"The value of a is 3";
+	break;
+	
+	default:
+		cout<<"default will be always printed";	 	
+  }
+
+}
+
+
